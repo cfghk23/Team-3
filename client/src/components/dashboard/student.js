e7ebd4ffbdeda7bf797f979a565fe0da7f60902e
rongxchen <chenrongxin20020102@gmail.com>
2023-10-06 12:01:41 +0800

    commit

---
+++ client/src/components/dashboard/student.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function StudentPage() {
+    const buttonStyle = {
+        backgroundColor: "white",
+        color: "black",
+        border: "none",
+        outline: "none",
+        padding: "10px 20px",
+        margin: "5px",
+        cursor: "pointer",
+        fontSize: "16px",
+    };
+
+    const buttonGroupStyle = {
+        display: "flex",
+        justifyContent: "center",
+    };
+
+    return (
+        <div className="dashboard">
+            {/* Create the button group */}
+            <div style={buttonGroupStyle}>
+                <button style={buttonStyle}>Button 1</button>
+                <button style={buttonStyle}>Button 2</button>
+                <button style={buttonStyle}>Button 3</button>
+            </div>
+
+            {/* Create the profile picture */}
+            <img src="path/to/profile/picture.jpg" className="profile-picture" />
+        </div>
+    );
+}
+
+export default StudentPage;
