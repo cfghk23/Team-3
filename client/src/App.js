2eef897db1882adcf96d6dd3c9f20b6e242f6552
Her Wei <hwlim@connect.ust.hk>
2023-10-06 14:34:59 +0800

    Update code format

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
-
+/* eslint-disable no-unused-vars */
+import React from "react"
+import "./style.css"
+import "tailwindcss/lib/css/preflight.css"
+import AnimationRevealPage from "./helpers/AnimationRevealPage"
+import Hero from "./components/hero/FullWidthWithImage"
 import Login from "./pages/Login"
 import BlogIndexPage from "./pages/BlogIndex"
 import ContestPage from "./pages/ContestPage"
